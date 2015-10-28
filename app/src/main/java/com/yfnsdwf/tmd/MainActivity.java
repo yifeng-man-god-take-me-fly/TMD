3a0bdf2cd1b00b00d798e8d380b0e7825994513a
Perqin <perqin@yeah.net>
2015-10-29 01:17:15 +0800

    Working on NewTaskActivity 1. Add task name and description EditText; 2. MainActivity can receive result from NewTaskActivity; 3. Add MyProfileActivity; 4. I18n.

--- app/src/main/java/com/yfnsdwf/tmd/MainActivity.java
+++ app/src/main/java/com/yfnsdwf/tmd/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private FloatingActionButton mAddTaskFab;
@@ -18,16 +19,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toolbar mToolbar = (Toolbar)findViewById(R.id.main_toolbar);
+        mToolbar.setNavigationIcon(R.drawable.ic_profile_white_24dp);
 
         mAddTaskFab = (FloatingActionButton)findViewById(R.id.add_task_fab);
         mAddTaskFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, NewTaskActivity.class));
+                Intent intent = new Intent(MainActivity.this, NewTaskActivity.class);
+                startActivityForResult(intent, NewTaskActivity.REQ_NEW_TASK);
             }
         });
 
         setSupportActionBar(mToolbar);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case NewTaskActivity.REQ_NEW_TASK:
+                // TODO
+                switch (requestCode) {
+                    case NewTaskActivity.RES_OK:
+                        Bundle extras = data.getExtras();
+                        String nam = extras.getString(NewTaskActivity.KEY_TASK_NAME);
+                        String des = extras.getString(NewTaskActivity.KEY_TASK_DESCRIPTION);
+                        Toast.makeText(this, nam + " ; " + des, Toast.LENGTH_LONG).show();
+                        break;
+                    case NewTaskActivity.RES_CANCEL:
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            default:
+                break;
+        }
     }
 
     @Override
@@ -38,10 +64,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent intent = new Intent(this, MyProfileActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
+        return true;
     }
 }
--- app/src/main/java/com/yfnsdwf/tmd/NewTaskActivity.java
+++ app/src/main/java/com/yfnsdwf/tmd/NewTaskActivity.java
@@ -8,9 +8,30 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 
 public class NewTaskActivity extends AppCompatActivity {
+    public static final int REQ_NEW_TASK = 0;
+
+    public static final int RES_OK = 0;
+    public static final int RES_CANCEL = 1;
+
+    public static final String KEY_TASK_NAME = "TASK_NAME";
+    public static final String KEY_TASK_DESCRIPTION = "TASK_DESCRIPTION";
+
     private Toolbar mToolbar;
+    private EditText mTaskNameEditText;
+    private EditText mTaskDescriptionEditText;
+
+    private void doneAddTask() {
+        Intent intent = new Intent();
+        Bundle extras = new Bundle();
+        extras.putString(KEY_TASK_NAME, mTaskNameEditText.getText().toString());
+        extras.putString(KEY_TASK_DESCRIPTION, mTaskDescriptionEditText.getText().toString());
+        intent.putExtras(extras);
+        setResult(RES_OK, intent);
+        finish();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +40,11 @@ public class NewTaskActivity extends AppCompatActivity {
 
         mToolbar = (Toolbar)findViewById(R.id.new_task_toolbar);
         mToolbar.setNavigationIcon(R.drawable.ic_done_white_24dp);
+
+        mTaskNameEditText = (EditText)findViewById(R.id.task_name_edit_text);
+
+        mTaskDescriptionEditText = (EditText)findViewById(R.id.task_description_edit_text);
+
         mToolbar.setTitle(R.string.title_activity_new_task);
 
         setSupportActionBar(mToolbar);
@@ -34,6 +60,7 @@ public class NewTaskActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
+                doneAddTask();
                 break;
             default:
                 return super.onOptionsItemSelected(item);
